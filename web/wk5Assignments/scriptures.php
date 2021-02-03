279f5fdde60e5e6a94855769895a1d15b2fa1d5a
BrowCow <cow18003@byui.edu>
2021-02-02 20:37:20 -0800

    display test3

--- web/wk5Assignments/scriptures.php
+++ web/wk5Assignments/scriptures.php
@@ -61,7 +61,7 @@ try {
   {
       echo $row['id'];
       // Code below added for team activity
-      $display = "<p><strong>Book:  $row[book] Chapter: $row[chapter] Verse: $row[verse]</strong>";
+      $display = "<p><strong>$row[book]" . " " . "$row[chapter]:$row[verse]</strong>";
       $display .= " - '$row[content]'</p>";
       echo $display;
   }
