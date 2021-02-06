24d66794aaf6511b331e7a5156ba97cbbfad9663
BrowCow <cow18003@byui.edu>
2021-02-06 09:05:55 -0800

    update all

--- web/wk5Assignments/phpDataAccess_client.php
+++ web/wk5Assignments/phpDataAccess_client.php
@@ -57,12 +57,12 @@ session_start();
                 array_push($clientNameArray, $row['client_lastname']);
                 array_push($clientNameArray, $row['client_email']);
                 array_push($clientNameArray, $row['client_phone']);
-            } else if($search_lastname == $row['client_lastname']) {
+            } /*else if($search_lastname == $row['client_lastname']) {
                 array_push($clientNameArray, $row['client_firstname']);
                 array_push($clientNameArray, $row['client_lastname']);
                 array_push($clientNameArray, $row['client_email']);
                 array_push($clientNameArray, $row['client_phone']);
-            }
+            }*/
         ?>
 
         <nav class="navbar navbar-expand-sm bg-light">
