cbd119456e9e5c477fa95473a6502a5ad44767c4
BrowCow <cow18003@byui.edu>
2021-02-06 19:24:19 -0800

    test

--- web/wk5Assignments/phpDataAccess_order.php
+++ web/wk5Assignments/phpDataAccess_order.php
@@ -40,11 +40,18 @@ session_start();
                 $statement = $db->prepare("SELECT order_type, order_date, public.orderitem.client_id, client_firstname, client_lastname FROM public.order INNER JOIN public.orderitem ON public.order.orderitem_id = public.orderitem.orderitem_id INNER JOIN public.client ON public.orderitem.client_id = public.client.client_id WHERE public.orderitem.client_id = public.client.client_id");
                 $statement->execute();
 
+
+                $orderArray = array();
+
                 if(isset($_POST['order_type'])){
                     $search_orderType = $_POST['order_type'];
-                    $orderTypeArray = array();
                 }
+
                 
+                if(null !==($_POST['year'] && $_POST['month'] && $_POST['day'])){
+                    $search_orderDate = $_POST['year'] . "-" . $_POST['month'] . "-" . $_POST['day'];
+                }
+                //echo $search_orderDate;
 
                 // Go through each result
                 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
@@ -61,18 +68,17 @@ session_start();
 
                 // Need to create an array and push results to it instead of variable
                 if($search_orderType == $row['order_type']) {
-                    array_push($orderTypeArray, $row['order_type']);
-                    array_push($orderTypeArray, $row['client_firstname']);
-                    array_push($orderTypeArray, $row['client_lastname']);
-                    array_push($orderTypeArray, $row['order_date']);
-                    
-                    //echo $row['client_firstname'] . "<br>";
-                    //$result_orderType = $row['order_type'];
-                    //$result_orderDate = $row['order_date'];
-                    //$result_firstName = $row['client_firstname'];
-                    //echo $row['client_lastname'] . "<br>";
-                    //$result_lastName = $row['client_lastname'];
+                    array_push($orderArray, $row['order_type']);
+                    array_push($orderArray, $row['client_firstname']);
+                    array_push($orderArray, $row['client_lastname']);
+                    array_push($orderArray, $row['order_date']);
                     }
+                if($search_orderDate == $row['order_date']) {
+                    array_push($orderArray, $row['order_type']);
+                    array_push($orderArray, $row['client_firstname']);
+                    array_push($orderArray, $row['client_lastname']);
+                    array_push($orderArray, $row['order_date']);
+                    } 
                 }  
             ?>
 
@@ -92,6 +98,7 @@ session_start();
                 <a class="nav-link" href="phpDataAccess_item.php">Item</a>
             </li>
             </ul>
+            <h1 class="gemHunter">Gem Hunter Designs</h1>
         </nav>
 
         <main>
@@ -100,15 +107,76 @@ session_start();
             <h2>Order Search</h2>
 
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" title="Order Search" name="orderSearch">
-                <div class="form-group">
-                    <label for="order_type">Order Type:</label>
-                    <?php if(isset($_SESSION['order_type'])): ?>
-                    <input type="text" class="form-control" id="order_type" name="order_type" value="<?php echo $_SESSION['order_type']?>">
-                    <?php else: ?>
-                    <input type="text" class="form-control" id="order_type" name="order_type">
-                    <?php endif; ?>
-                </div>
 
+                <label for="order_type">Search by payment type:</label>
+                <br>
+                <select id="order_type" name="order_type">
+                    <option value="">Select</option>
+                    <option value="Online">Online</option>
+                    <option value="Credit">Credit</option>
+                    <option value="Cash">Cash</option>
+                </select>
+                <br>
+                <p>Search by order date:</p>
+                <label for="month">Month</label>
+                <select id="month" name="month">
+                    <option value=""></option>
+                    <option value="01">January</option>
+                    <option value="02">February</option>
+                    <option value="03">March</option>
+                    <option value="04">April</option>
+                    <option value="05">May</option>
+                    <option value="06">June</option>
+                    <option value="07">July</option>
+                    <option value="08">August</option>
+                    <option value="09">September</option>
+                    <option value="10">October</option>
+                    <option value="11">November</option>
+                    <option value="12">December</option>
+                </select>
+                <label for="day">Day</label>
+                <select id="day" name="day">
+                    <option value=""></option>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                    <option value="03">03</option>
+                    <option value="04">04</option>
+                    <option value="05">05</option>
+                    <option value="06">06</option>
+                    <option value="07">07</option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+                </select>
+                <label for="year">Year</label>
+                <select id="year" name="year">
+                    <option value=""></option>
+                    <option value="2019">2019</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                </select>
+                <br>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
             
@@ -116,7 +184,7 @@ session_start();
             <br>
             <br>
             <?php 
-                if(count($orderTypeArray) > 0){
+                if(count($orderArray) > 0){
                     echo "<h3>" . $search_orderType . " Orders</h3>";
                     echo "<table class='table table-bordered'>";
                     echo "<thead>";
@@ -130,17 +198,17 @@ session_start();
                     echo "<tbody>";
                 }
 
-                $orderArrayCount = count($orderTypeArray);
+                $orderArrayCount = count($orderArray);
 
                 for ($x = 0; $x <= $orderArrayCount; $x++) {
                     echo "<tr>";
-                    echo "<td>$orderTypeArray[$x]</td>"; 
+                    echo "<td>$orderArray[$x]</td>"; 
                     $x++;
-                    echo "<td>$orderTypeArray[$x]</td>"; 
+                    echo "<td>$orderArray[$x]</td>"; 
                     $x++;
-                    echo "<td>$orderTypeArray[$x]</td>"; 
+                    echo "<td>$orderArray[$x]</td>"; 
                     $x++;
-                    echo "<td>$orderTypeArray[$x]</td>"; 
+                    echo "<td>$orderArray[$x]</td>"; 
                     echo "</tr>"; 
                 }
                   
@@ -148,13 +216,8 @@ session_start();
                 echo "</table>";
             ?>
 
-
-
-
-
         </main>
     
-
     </body>
 
 </html>
