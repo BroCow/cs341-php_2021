4e50c583a3393c15cc44ef5699a30701876a7e18
BrowCow <cow18003@byui.edu>
2021-02-08 14:49:40 -0800

    6prep

--- web/wk6Assignments/teach_insertScripture.php
+++ web/wk6Assignments/teach_insertScripture.php
@@ -19,15 +19,31 @@
         
         <form method="post" action="scripture_topic.php">
             <input type="hidden" name="course_id" value="<?php echo $course_id; ?>">
-            <input type="text" name="book">
-            <input type="text" name="chapter">
-            <input type="text" name="verse">
-            <textarea name="content"></textarea>
-
+            <div class="form-group">
+                <label for="book">Book:</label>
+                <input class="form-control" type="text" name="book">
+            </div>
+            <div class="form-group">
+                <label for="chapter">Chapter:</label>
+                <input class="form-control" type="text" name="chapter">
+            </div>
+            <div class="form-group">
+                <label for="verse">Verse:</label>
+                <input class="form-control" type="text" name="verse">
+            </div>
+            <div class="form-group">
+                <label for="content">Scripture:</label>
+                <textarea class="form-control" name="scripture">
+            </div>
+            
             <p>Select a topic below that corresponds with the scripure</p>
             <?php foreach($name_rows as $name_row){
                 $topic = $name_row['name'];
-                echo $topic;
+                echo "<div class='form-check'>";
+                echo "<label class='form-check-label'>";
+                echo "<input type='checkbox' class='form-check-input' name='cont1' value='<?php echo $topic ?>'>echo $topic";
+                echo "</label>";
+                echo "</div>";
             }
             ?>
 
