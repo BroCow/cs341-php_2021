3ce8d57d3863e3b542db5efe7dea8a423347c477
BrowCow <cow18003@byui.edu>
2021-02-07 17:48:48 -0800

    wk6Prepare

---
+++ web/wk6Assignments/prep_dbconnectQuery.php
@@ -0,0 +1,32 @@
+<?php
+require('dbConnect.php');
+$db = get_db();
+
+//$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+//$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+//$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+//$stmt->execute();
+//$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$query = 'SELECT id, code, name FROM course';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$courses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Courses</title>
+    </head>
+    <body>
+        <h1>Courses</h1>
+
+        <ul>
+            <li><p></p></li>
+            <li><p></p></li>
+        </ul>
+    </body>
+</html>
