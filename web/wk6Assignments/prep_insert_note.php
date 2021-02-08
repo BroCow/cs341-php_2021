4a814a16e394b6ffd1d719564eeec1d7a206e82f
BrowCow <cow18003@byui.edu>
2021-02-08 12:26:22 -0800

    6prep

--- web/wk6Assignments/prep_insert_note.php
+++ web/wk6Assignments/prep_insert_note.php
@@ -3,8 +3,19 @@
 $course_id = htmlspecialchars($_POST['course_id']);
 $note_content = htmlspecialchars($_POST['note_content']);
 
-echo "$course_id\n";
-echo $note_content;
+require('dbConnect.php');
+$db = get_db();
+
+$stmt = $db->prepare('INSERT INTO note (course_id, content) VALUES (:course_id, :content);');
+$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+$stmt->bindValue(':note_content', $note_content, PDO::PARAM_STR);
+$stmt->execute();
+
+
+$new_page = "prep_coursenotes.php?id=$course_id";
+
+header("Location: $new_page");
+die();
 
 
 ?>
