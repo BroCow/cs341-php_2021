5a52b070a9b3c86ecb0512ca3187703b4f97f5d1
BrowCow <cow18003@byui.edu>
2021-02-08 12:09:54 -0800

    6prep

--- web/wk6Assignments/prep_insert_note.php
+++ web/wk6Assignments/prep_insert_note.php
@@ -14,23 +14,14 @@ echo $note_content;
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Course Notes</title>
+        <title></title>
     </head>
     <body>
-        <h1>Course Notes for <?php echo $course_code ?></h1>
+        <h1></h1>
 
-        <?php
-        foreach ($note_rows as $note_row){
-            $content = $note_row['content'];
-            echo "<p>$content</p>";
-        }
-        ?>
         
-        <form method="post" action="insert_note.php">
-            <textarea name="note_content"></textarea>
-            <input type="submit" value="Create Note">
-            <input type="hidden" name="course_id" value="<?php echo $course_id; ?>">
-        </form>
+        
+        
         
     </body>
 </html>
