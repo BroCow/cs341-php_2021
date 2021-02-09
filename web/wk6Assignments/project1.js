d37563167f718607d8338b45eda8883e5a3437f3
BrowCow <cow18003@byui.edu>
2021-02-09 15:52:47 -0800

    testPage

--- web/wk6Assignments/project1.js
+++ web/wk6Assignments/project1.js
@@ -1,10 +1,5 @@
 
 
 function clientSearch () {
-    var x = document.getElementById('hideSearch');
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    document.getElementById('hideSearch').style.display = "block";
 }
