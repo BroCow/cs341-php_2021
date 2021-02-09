5c8d815348a35305f175ac4c757a337ce48d4fd5
BrowCow <cow18003@byui.edu>
2021-02-09 15:45:13 -0800

    testPage

--- web/wk6Assignments/project1.js
+++ web/wk6Assignments/project1.js
@@ -1,12 +1,10 @@
-var clientSearch = "<?php echo $clientSearch ?>";
-
-if(clientSearch == false){
-    document.getElementById('h2_clientSearch').style.display = "none";
-    document.getElementById('form_clientSearch').style.display = "none";
-}
 
 
 function clientSearch () {
-    document.getElementById('h2_clientSearch').style.display = "block";
-    document.getElementById('form_clientSearch').style.display = "block";
+    var x = document.getElementById('hideSearch');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
