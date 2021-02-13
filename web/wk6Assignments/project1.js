e98bef4fa67de43b05b3e736d68e9f765eade11f
BrowCow <cow18003@byui.edu>
2021-02-13 10:07:34 -0800

    testPage

--- web/wk6Assignments/project1.js
+++ web/wk6Assignments/project1.js
@@ -26,3 +26,30 @@ function toggleClientSearch() {
       x.style.display = "none";
     }
   }
+/********** Item Page Functions  **************/
+  function toggleItemSearch() {
+    var x = document.getElementById("itemSearchForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleItemAdd() {
+    var x = document.getElementById("itemAddForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function toggleItemDelete() {
+    var x = document.getElementById("itemDeleteForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
