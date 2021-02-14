93a30c0c0b5fb632eba59c87529c06d08c7eb432
BrowCow <cow18003@byui.edu>
2021-02-13 17:14:33 -0800

    testPage

--- web/wk6Assignments/phpDataAccess_order6.php
+++ web/wk6Assignments/phpDataAccess_order6.php
@@ -37,10 +37,11 @@ session_start();
     <!-- If/else statements for each possible order type selection with different statement prepared for each?-->
     <body>
         <?php
+            if(isset($_POST['order_type']) || isset($_POST['month'])){
                 $statement = $db->prepare("SELECT order_type, order_date, public.orderitem.client_id, client_firstname, client_lastname FROM public.order INNER JOIN public.orderitem ON public.order.orderitem_id = public.orderitem.orderitem_id INNER JOIN public.client ON public.orderitem.client_id = public.client.client_id WHERE public.orderitem.client_id = public.client.client_id");
                 $statement->execute();
 
-
+ 
                 $orderArray = array();
 
                 if(isset($_POST['order_type'])){
@@ -79,8 +80,29 @@ session_start();
                     array_push($orderArray, $row['client_lastname']);
                     array_push($orderArray, $row['order_date']);
                     } 
-                }  
-            ?>
+                } 
+            }
+
+
+            if(isset($_POST['Addorder_type'])){ 
+                
+                $AddOrderType = htmlspecialchars($_POST['Addorder_type']);
+                $AddOrderMonth = htmlspecialchars($_POST['add_month']);
+                $AddOrderDay = htmlspecialchars($_POST['add_day']);
+                $AddOrderYear = htmlspecialchars($_POST['add_year']);
+
+                $AddOrderDate = $AddOrderYear . "-" . $AddOrderMonth . "-" . $AddOrderDay;
+                echo $AddOrderDate;
+                
+                $query = "INSERT INTO public.order (order_date, order_type) VALUES (:AddOrderDate, :AddOrderType)";
+                
+                $stmt = $db->prepare($query);
+                $stmt->bindValue(':AddOrderDate', $AddOrderDate, PDO::PARAM_STR);
+                $stmt->bindValue(':AddOrderType', $AddOrderType, PDO::PARAM_STR);
+                $stmt->execute();
+            }
+            
+        ?>
 
         <nav class="navbar navbar-expand-sm bg-light">
             <!-- Links -->
@@ -104,81 +126,185 @@ session_start();
         <main>
             <h1>Order Management</h1>
 
-            <h2>Order Search</h2>
+            <div id="test" class="container">
+                <div class="row">
+                    <div class="col">
+                            <button onclick="toggleOrderSearch()" id="OrderSearch" class="homeButton">Search</button>
+                    </div>
 
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" title="Order Search" name="orderSearch">
+                    <div class="col">
+                            <button onclick="toggleOrderAdd()" id="OrderAdd" class="homeButton">Add</button>
+                    </div>
 
-                <label for="order_type">Search by payment type:</label>
+                    <div class="col">
+                            <button onclick="toggleOrderDelete()" id="orderDelete" class="homeButton">Delete</button>
+                    </div>
+                </div>
+            </div>
+
+            <div id="orderSearchForm" style="display:none;">
+                <h2>Order Search</h2>
                 <br>
-                <select id="order_type" name="order_type">
-                    <option value="">Select</option>
-                    <option value="Online">Online</option>
-                    <option value="Credit">Credit</option>
-                    <option value="Cash">Cash</option>
-                </select>
                 <br>
-                <p>Search by order date:</p>
-                <label for="month">Month</label>
-                <select id="month" name="month">
-                    <option value=""></option>
-                    <option value="01">January</option>
-                    <option value="02">February</option>
-                    <option value="03">March</option>
-                    <option value="04">April</option>
-                    <option value="05">May</option>
-                    <option value="06">June</option>
-                    <option value="07">July</option>
-                    <option value="08">August</option>
-                    <option value="09">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                </select>
-                <label for="day">Day</label>
-                <select id="day" name="day">
-                    <option value=""></option>
-                    <option value="01">01</option>
-                    <option value="02">02</option>
-                    <option value="03">03</option>
-                    <option value="04">04</option>
-                    <option value="05">05</option>
-                    <option value="06">06</option>
-                    <option value="07">07</option>
-                    <option value="08">08</option>
-                    <option value="09">09</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-                </select>
-                <label for="year">Year</label>
-                <select id="year" name="year">
-                    <option value=""></option>
-                    <option value="2019">2019</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
-                </select>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" title="Order Search" name="orderSearch">
+
+                    <label for="order_type">Search by payment type:</label>
+                    <br>
+                    <select id="order_type" name="order_type">
+                        <option value="">Select</option>
+                        <option value="Online">Online</option>
+                        <option value="Credit">Credit</option>
+                        <option value="Cash">Cash</option>
+                    </select>
+                    <br>
+                    <p>Search by order date:</p>
+                    <label for="month">Month</label>
+                    <select id="month" name="month">
+                        <option value=""></option>
+                        <option value="01">January</option>
+                        <option value="02">February</option>
+                        <option value="03">March</option>
+                        <option value="04">April</option>
+                        <option value="05">May</option>
+                        <option value="06">June</option>
+                        <option value="07">July</option>
+                        <option value="08">August</option>
+                        <option value="09">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
+                    </select>
+                    <label for="day">Day</label>
+                    <select id="day" name="day">
+                        <option value=""></option>
+                        <option value="01">01</option>
+                        <option value="02">02</option>
+                        <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08</option>
+                        <option value="09">09</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                        <option value="26">26</option>
+                        <option value="27">27</option>
+                        <option value="28">28</option>
+                        <option value="29">29</option>
+                        <option value="30">30</option>
+                        <option value="31">31</option>
+                    </select>
+                    <label for="year">Year</label>
+                    <select id="year" name="year">
+                        <option value=""></option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                        <option value="2021">2021</option>
+                    </select>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Search</button>
+                </form>
+            </div>
+
+            <div id="orderAddForm" style="display:none;">
                 <br>
-                <button type="submit" class="btn btn-primary">Search</button>
-            </form>
+                <br>
+                <h2>Add Order</h2>
+                <form id="form_orderAdd" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" title="Order Add" name="orderAdd">
+                    
+                    <div class="form-group">
+                        <p>Select Order Date:</p>
+                        <label for="add_month">Month</label>
+                        <select id="add_month" name="add_month">
+                            <option value=""></option>
+                            <option value="01">January</option>
+                            <option value="02">February</option>
+                            <option value="03">March</option>
+                            <option value="04">April</option>
+                            <option value="05">May</option>
+                            <option value="06">June</option>
+                            <option value="07">July</option>
+                            <option value="08">August</option>
+                            <option value="09">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+                        <label for="add_day">Day</label>
+                        <select id="add_day" name="add_day">
+                            <option value=""></option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                        <label for="add_year">Year</label>
+                        <select id="add_year" name="add_year">
+                            <option value=""></option>
+                            <option value="2019">2019</option>
+                            <option value="2020">2020</option>
+                            <option value="2021">2021</option>
+                        </select>
+                        <br>
+                    </div>
+                
+                    <div class="form-group">
+                        <label for="Addorder_type">Select order type to add:</label>
+                        <br>
+                        <select id="Addorder_type" name="Addorder_type" required>
+                            <option value="">Select</option>
+                            <option value="Online">Online</option>
+                            <option value="Cash">Cash</option>
+                            <option value="Credit">Credit</option>
+                        </select>
+                    </div>
+
+                    <button type="submit" class="btn-lg btn-primary">Add Order</button>
+
+                </form>
+            </div>
             
             <!-- Make this a table for each one -->
             <br>
@@ -217,7 +343,7 @@ session_start();
             ?>
 
         </main>
-    
+        <script src="project1.js"></script>
     </body>
 
 </html>
