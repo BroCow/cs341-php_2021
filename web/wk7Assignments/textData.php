39e99732cc3ae09a8cb31e4beba53f89f32e53f9
BrowCow <cow18003@byui.edu>
2021-02-20 16:54:49 -0800

    order add update

--- web/wk7Assignments/textData.php
+++ web/wk7Assignments/textData.php
@@ -125,7 +125,7 @@ session_start();
                         array_push($orderNameArray, $row['client_email']);
                         array_push($orderNameArray, $row['client_phone']);
                     } else if ($order_firstname != $row['client_firstname'] || $order_lastname != $row['client_lastname']) {
-                        echo "Client not found";
+                        $_SESSION['addOrderClientNotFound'] = "Client not found";
                     }
 
                 }
@@ -378,6 +378,9 @@ session_start();
             <form id="form_orderAddConfirmClient" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" title="Confirm Client" name="orderAddConfirmClient">
                 <?php 
                     if(isset($_POST['Addorder_firstname']) || isset($_POST['Addorder_lastname'])){
+                        if (empty($_SESSION['addOrderClientNotFound'])) {
+                            echo "It is empty";
+                        }
                         echo "<h3>After verifying client information, check the box and then select <q>Confirm Client</q></h3>";
                         
                         $orderNameArrayCount = count($orderNameArray);
